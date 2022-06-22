1ace58d233383828bd952f371ed451ac88bb806a
Denis <denis.soldugeev@gmail.com>
2022-06-23 01:18:15 +0300

    Blog-MERN-BACKEND

---
+++ controllers/PostController.js
@@ -0,0 +1,130 @@
+import PostModel from "../model/Post.js";
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось создать статью",
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Не удалась вернуть статью",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Не удалась удалить статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.update(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags.split(","),
+      }
+    );
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
+    });
+  }
+};
---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+import { registerValidation, loginValidation } from "./validations/auth.js";
+import checkAuth from "./utils/checkAuth.js";
+import handleValidationsErrors from "./utils/handleValidationsErrors.js";
+import { postCreateValidation } from "./validations/post.js";
+import { register, login, getMe } from "./controllers/UserContoroller.js";
+import {
+  create,
+  getAll,
+  getOne,
+  remove,
+  update,
+} from "./controllers/PostController.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.dac0w.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationsErrors,
+  register
+);
+app.post("/auth/login", loginValidation, handleValidationsErrors, login);
+app.get("/auth/me", checkAuth, getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationsErrors,
+  create
+);
+app.get("/posts", getAll);
+app.get("/posts/:id", getOne);
+app.delete("/posts/:id", checkAuth, remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationsErrors,
+  update
+);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
